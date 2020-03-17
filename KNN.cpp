76437b0002536fb66df76f0246fd2f551719111f
caofulin <caofulin@minieye.cc>
2020-03-17 17:39:46 +0800

    update

--- KNN.cpp
+++ KNN.cpp
@@ -2,20 +2,92 @@
 // Created by cao on 2020/3/16.
 //
 
+#include <cassert>
 #include "KNN.h"
+#include <cstdio>
+#include <cstring>
 
 void KNN::fit(std::vector<std::vector<float> > data, std::vector<int> lables) {
-
+    assert(data.size() == lables.size());
+    int data_len = data.size();
+    KNN::n = data_len;
+    KNN::m = data[0].size();
+    for(int i = 0; i < data_len; i++){
+        KNN::root = insert(KNN::root, data[i], lables[i], 0);
+    }
 }
 
 std::vector<int> KNN::predict(std::vector<std::vector<float> > data) {
 
+    std::vector<int> res;
+    if(!KNN::root) {
+        printf("not fit");
+        return res;
+    }
+    int data_len = data.size();
+    int labels[KNN::c];
+    for(int i = 0; i < data_len; i++ ){
+        KNN::search_knn(KNN::root, data[i], 0);
+        memset(labels, 0, sizeof(labels));
+        while(!KNN::pq.empty()) {
+            KNN::PQ_Node now = KNN::pq.top();
+            KNN::pq.pop();
+            labels[now.label] += 1;
+            printf("%d, %f\n", now.label, now.dis);
+        }
+        int mx = -1, l = -1;
+        for(int j = 0; j < KNN::c; j++) {
+            printf("labels, %d %d\n", j, labels[j]);
+            if(labels[j] > mx) mx = labels[j], l = j;
+        }
+        res.push_back(l);
+    }
+    return res;
 }
 
-KNN::KNN(int k) {
+KNN::KNN(int k, int c) {
     KNN::k = k;
+    KNN::c = c;
 }
 
-void KNN::insert() {
+float KNN::cal_distance(std::vector<float> v_a, std::vector<float> v_b) {
+    assert( v_a.size() == v_b.size());
+    float dis = 0.0;
+    int len = v_a.size();
+    for( int i = 0; i < len; i++) {
+        dis += (v_a[i] - v_b[i]) * (v_a[i] - v_b[i]);
+    }
+    return dis;
+}
 
+KNN::KD_Node* KNN::insert(KNN::KD_Node* r, std::vector<float> vec, int label, int idx) {
+    if(!r) return KNN::create_kd_node(vec, label);
+    idx = idx%KNN::m;
+    if(vec[idx] < r->vec[idx]) r->left = insert(r->left, vec, label, idx+1);
+    else r->right = insert(r->right, vec, label, idx+1);
+    return r;
+}
+
+KNN::KD_Node *KNN::create_kd_node(std::vector<float> vec, int label) {
+    KD_Node* res = new KD_Node();
+    res->vec = vec;
+    res->label = label;
+    return res;
+}
+
+void KNN::search_knn(KNN::KD_Node *r, std::vector<float> vec, int idx) {
+    if(!r) return;
+    float now_dis = KNN::cal_distance(vec, r->vec);
+    KNN::pq.push(KNN::PQ_Node(now_dis, r->label, r->vec));
+    if(KNN::pq.size() > k)  KNN::pq.pop();
+    idx = idx%KNN::m;
+    bool flag = pq.top().dis > (vec[idx] - r->vec[idx])*(vec[idx] - r->vec[idx]);
+    if(vec[idx] < r->vec[idx]) {
+        search_knn(r->left, vec, idx+1);
+        if(flag) search_knn(r->right, vec, idx+1);
+    }
+    else {
+        search_knn(r->right, vec, idx+1);
+        if(flag) search_knn(r->left, vec, idx+1);
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "KMEANS.h"
 #include <cstdlib>
+#include "KNN.h"
 using namespace std;
 
 
@@ -25,10 +26,31 @@ int main() {
         cout << endl;
     }
     vector<int > res = kmeans.predict(data);
+    for(int i = 0; i < 10; i++) cout << res[i] << " ";
+    cout << endl;
 
-    for(int i = 0; i < 10; i++){
-        cout << res[i] << endl;
+    KNN knn = KNN(2, 2);
+
+    vector< vector<float > > t_data(4);
+    vector<int> labels;
+    for (int l = 0; l < 2; ++l) {
+        for (int i = 0; i < 2; ++i) {
+            t_data[l*2+i].push_back(l);
+            t_data[l*2+i].push_back(i);
+        }
     }
+    labels.push_back(0);
+    labels.push_back(0);
+    labels.push_back(1);
+    labels.push_back(1);
 
+    knn.fit(t_data, labels);
+    t_data[3].clear();
+    t_data[3].push_back(3);
+    t_data[3].push_back(2);
+    labels = knn.predict(t_data);
+    for (int m = 0; m < 4 ; ++m) {
+        cout << labels[m] << " ";
+    }
     return 0;
 }

e5ea43f62cfdf7d8096d5c4ae1d59175a793306b
fulincao <1343497749@qq.com>
2021-12-20 11:06:24 +0800

    2021-12-20-11-06-24

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211220.1104';
+const cacheName = 'chirpy-20211220.1106';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {

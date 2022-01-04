65ab224e0888dbac575bbd870289f15980ebe2a7
fulincao <1343497749@qq.com>
2022-01-04 20:32:08 +0800

    2022-01-04-20-32-08

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211227.1803';
+const cacheName = 'chirpy-20220104.2032';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {

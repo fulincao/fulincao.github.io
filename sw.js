d5be7c2aca323a0b88c6a6588b9537b2a0abe479
fulincao <fulinao@autox.ai>
2022-08-11 18:08:54 +0800

    2022-08-11-18-08-54

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220805.1740';
+const cacheName = 'chirpy-20220811.1808';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
